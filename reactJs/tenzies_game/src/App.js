30af0df0c22815d51bd44ed14f5196033259b77e
Parmeet Singh <parmeet1655.cse18@chitkara.edu.in>
2022-12-20 00:30:56 +0530

    add holding dice functionality

--- reactJs/tenzies_game/src/App.js
+++ reactJs/tenzies_game/src/App.js
@@ -4,11 +4,17 @@ import { nanoid } from 'nanoid'
 
 function App() {
   const [dice,setDice] = useState(allNewDice())
+  
+ 
   // roll button re-generating random numbers
   function rollDice(){
-    setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die =>{
+      return die.isHeld ? die : {...die, value:Math.ceil(Math.random()*6)}
+    }))
   }
-  // return an object newDice with random numbers generated on 10 dices
+
+  // return an array of objects with random 
+  //numbers generated on 10 dices
   function allNewDice(){
     const newDice = []
 
@@ -25,9 +31,15 @@ function App() {
     return newDice
   }
 
+  function holdDice(id){
+    setDice(oldDice => oldDice.map(die => {
+      return  die.id===id ?  {...die, isHeld: !die.isHeld}: die
+    }))
+  }
+
   
 
-  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld}/>)
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
 
   return (
     <main className="app--main">
