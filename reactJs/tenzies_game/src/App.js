fab4e76fb008ff282adde556899913509d925734
Parmeet Singh <parmeet1655.cse18@chitkara.edu.in>
2022-12-19 19:26:56 +0530

    add roll dice button and generate random numbers on them

--- reactJs/tenzies_game/src/App.js
+++ reactJs/tenzies_game/src/App.js
@@ -1,23 +1,41 @@
+import { useState } from "react";
 import Die from "./Components/Die";
 
 
 function App() {
+  const [dice,setDice] = useState(allNewDice())
+  // roll button re-generating random numbers
+  function rollDice(){
+    setDice(allNewDice())
+  }
+console.log(dice)
+  // return an object newDice with random numbers generated on 10 dices
+  function allNewDice(){
+    const newDice = []
+
+    for(let i=0;i<10;i++){
+      const randomValue = Math.ceil(Math.random()*6)
+      newDice.push(
+        {
+          value: randomValue,
+          isHeld: false
+        })
+    }
+    return newDice
+  }
+
+  
+
+  const diceElements = dice.map((die,index) => <Die key={index} value={die.value}/>)
+
   return (
     <main className="app--main">
       <div className="dice--container">
-        <Die value={1}/>
-        <Die value={2}/>
-        <Die value={3}/>
-        <Die value={4}/>
-        <Die value={5}/>
-        <Die value={6}/>
-        <Die value={3}/>
-        <Die value={2}/>
-        <Die value={1}/>
-        <Die value={5}/>
+        {diceElements}
       </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
-  );
+  )
 }
 
 export default App;
