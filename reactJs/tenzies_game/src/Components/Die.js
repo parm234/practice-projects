e7eb668ba26a0d6c7cc123ee158adefbef9268b4
Parmeet Singh <parmeet1655.cse18@chitkara.edu.in>
2022-12-20 02:34:43 +0530

    add dots on the dices

--- reactJs/tenzies_game/src/Components/Die.js
+++ reactJs/tenzies_game/src/Components/Die.js
@@ -1,15 +1,22 @@
+import Dots from "./Dot"
 
 export default function Die(props){
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
     
+    let dots =[] 
+        for(let i =0;i<props.value;i++)
+        {
+            dots.push(<Dots/>)
+        }
+    
     return (
         <div className="die--face" 
             style={styles}
             onClick={props.holdDice}
         >
-            {props.value}
+            {dots}
         </div>
     )
 }
