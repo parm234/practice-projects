90f0f9c98e12cdd9beb803e8e9f9f0fff20bb34f
Parmeet Singh <parmeet1655.cse18@chitkara.edu.in>
2022-12-19 15:19:24 +0530

    add dices UI

--- reactJs/tenzies_game/src/App.js
+++ reactJs/tenzies_game/src/App.js
@@ -1,10 +1,22 @@
+import Die from "./Components/Die";
 
 
 function App() {
   return (
-    <div className="App">
-      hbv
-    </div>
+    <main className="app--main">
+      <div className="dice--container">
+        <Die value={1}/>
+        <Die value={2}/>
+        <Die value={3}/>
+        <Die value={4}/>
+        <Die value={5}/>
+        <Die value={6}/>
+        <Die value={3}/>
+        <Die value={2}/>
+        <Die value={1}/>
+        <Die value={5}/>
+      </div>
+    </main>
   );
 }
 
---
+++ reactJs/tenzies_game/src/Components/Die.js
@@ -0,0 +1,8 @@
+
+export default function Die(props){
+    return (
+        <div className="die--face">
+            {props.value}
+        </div>
+    )
+}
