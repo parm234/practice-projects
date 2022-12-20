b6438c1a84380a100133a20567da1ccdbdeba42c
Parmeet Singh <parmeet1655.cse18@chitkara.edu.in>
2022-12-21 01:47:06 +0530

    add styling to the landing page

---
+++ reactJs/quizzical/src/App.js
@@ -0,0 +1,13 @@
+import bg1 from './Images/bg1.png'
+import bg2 from './Images/bg2.png'
+function App() {
+  return (
+    <main>
+        <h1>Quizzical</h1>
+        <p>Let's see how smart you are :)</p>
+        <button className='enter-quiz-btn'>Start Quiz</button>
+    </main>
+  );
+}
+
+export default App;
